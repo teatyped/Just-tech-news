a2e9b025c8c7734af1e197f539de16395ca26e57
Khai Truong <kminht@gmail.com>
2022-05-17 16:42:47 -0400

    hide pass

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,13 +1,7 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
+const userRoutes = require('./user-routes.js');
 
-router.use('/user', userRoutes);
-
-router.use('/api', apiRoutes)
-
-router.use((req, res) => {
-    res.status(404).end();
-});
+router.use('/users', userRoutes);
 
 module.exports = router;
