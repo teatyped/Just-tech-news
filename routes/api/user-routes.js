fb0e46b6ff29d6dd5a8ab9d12ddd633343e5ecd5
Khai Truong <kminht@gmail.com>
2022-05-16 23:27:15 -0400

    crud started

---
+++ routes/api/user-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {
+    // access our user model and run findall method
+    User.findAll()
+    .then( dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+
+});
+
+// POST /api/users
+router.post('/', (req, res) => {
+
+});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {
+
+});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
